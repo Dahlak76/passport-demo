1745156b8a9a35d5aee3832fd40f5be0e5990e70
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-07-23 15:21:44 -0500

    compleet week 13 lesson 13

---
+++ app.js
@@ -0,0 +1,150 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const app = express();
+const bcrypt = require('bcryptjs')
+
+
+require('dotenv').config();
+const mongoDb = 
+`mongodb+srv://dahlak:${encodeURIComponent(process.env.DB_PASS)}@${process.env.DB_HOST}/PASSWORD-DEMO?retryWrites=true&w=majority`
+
+const MongoDBStore = require('connect-mongodb-session')(session)
+
+var store = new MongoDBStore({
+  uri: mongoDb,
+  collection: 'sessions'
+});
+
+// Catch errors
+store.on('error', function (error) {
+  console.log(error);
+});
+
+//const mongoDb = "YOUR MONGO URL HERE";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+  })
+);
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          return done(null, user)
+        } else {
+          console.log('message failed')
+          return done(null, false, { message: "Incorrect password" })
+        }
+      })
+
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  console.log('serialize user')
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log('deserialize user')
+  User.findById(id, function(err, user) {
+    //console.log(user)
+    done(err, user);
+  });
+});
+
+app.use(session({
+  secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true,
+  store: store
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+const authMiddleware = (req, res, next) => {
+  if (!req.user) {
+    if (!req.session.messages) {
+      req.session.messages = []
+    }
+    req.session.messages.push("You can't access that page before logon.")
+    res.redirect('/')
+  } else {
+    next()
+  }
+}
+
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureMessage: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  let messages = []
+  if (req.session.messages) {
+    messages = req.session.messages
+    req.session.messages = []
+  }
+  res.render("index", { messages });
+});
+
+app.get('/restricted', authMiddleware, (req, res) => {
+  if (!req.session.pageCount) {
+    req.session.pageCount = 1
+  } else {
+    req.session.pageCount++
+  }
+  res.render('restricted', { pageCount: req.session.pageCount })
+})
+
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    hashedPassword = await bcrypt.hash(req.body.password, 10)
+    console.log(hashedPassword)
+    await User.create({ username: req.body.username, password: hashedPassword })
+    res.redirect("/");
+  } catch (err) {
+    return next(err)
+  }
+});
+
+app.get("/log-out", (req, res) => {
+  req.session.destroy(function (err) {
+    res.redirect("/")
+  })
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
+
